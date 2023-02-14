d1a5891775dd3412fb57beb8f8312c4a0b1ea85e
tutu <227384408@qq.com>
2023-02-14 22:31:49 +0800

    build(axios): finish config

--- src/service/request/type.ts
+++ src/service/request/type.ts
@@ -1,22 +1,12 @@
 import type { AxiosRequestConfig, AxiosResponse } from 'axios'
-
-<<<<<<< HEAD
 export interface TURequestInterceptors<T = AxiosResponse> {
-=======
-export interface HYRequestInterceptors<T = AxiosResponse> {
->>>>>>> ae2fb9a90e0d4928ef009b88d7b3568f52033c67
   requestInterceptor?: (config: AxiosRequestConfig) => any
   requestInterceptorCatch?: (error: any) => any
   responseInterceptor?: (res: T) => T
   responseInterceptorCatch?: (error: any) => any
 }
 
-<<<<<<< HEAD
 export interface TURequestConfig<T = AxiosResponse> extends AxiosRequestConfig {
   interceptors?: TURequestInterceptors<T>
-=======
-export interface HYRequestConfig<T = AxiosResponse> extends AxiosRequestConfig {
-  interceptors?: HYRequestInterceptors<T>
->>>>>>> ae2fb9a90e0d4928ef009b88d7b3568f52033c67
   showLoading?: boolean
 }
